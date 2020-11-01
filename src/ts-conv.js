f550d7df10d6f61b550b93e7d4e2548aa34b5876
Matthias Gaertner <matthias.gaertner@infor.com>
2020-11-01 21:22:01 +0100

    Clipboad support.
    Shortened manifest description.
    Do not apply if web page already contains element with _our_ id.
    Allow copying to clipboard, with visual feedback icon.
    Make pop-up slightly translucent.

--- src/ts-conv.js
+++ src/ts-conv.js
@@ -1,36 +1,60 @@
 // https://github.com/m9aertner/ts-converter
-var timestamp = document.createElement('div');
-timestamp.setAttribute('class', 'timestamp_bg');
+(function() {
+  var timestamp = document.getElementById('timestamp_gmt');
+  if(!!timestamp) {
+    console.log("Timestamp Converter GMT: timestamp_gmt element already present, retreat.")
+    return;
+  }
 
-document.addEventListener('mouseup', function(e) {
-
+  var timestamp = document.createElement('div');
+  timestamp.setAttribute("id", "timestamp_gmt");
+  timestamp.setAttribute('class', 'timestamp_gmt timestamp_icn');
   document.body.appendChild(timestamp);
 
-  var selection = window.getSelection();
-  if (!!selection)  {
-    selection = selection.toString();
-    if (!isNaN(selection))  {
-      if (selection.length === 10) {
-        var decodedTS = new Date(JSON.parse(selection) * 1000).toISOString();
-        showTimestamp(e.pageX, e.pageY, decodedTS);
-      } else if (selection.length === 13) {
-        var decodedTS = new Date(JSON.parse(selection)).toISOString();
-        showTimestamp(e.pageX, e.pageY, decodedTS);
+  document.addEventListener('mouseup', function (e) {
+    if (!e.target.matches('.timestamp_gmt')) {
+      var selection = window.getSelection();
+      if (!!selection) {
+        selection = selection.toString();
+        if (!isNaN(selection)) {
+          if (selection.length === 10) {
+            var decodedTS = new Date(JSON.parse(selection) * 1000).toISOString();
+            showTimestampGMT(timestamp, e.pageX, e.pageY, decodedTS, selection);
+          } else if (selection.length === 13) {
+            var decodedTS = new Date(JSON.parse(selection)).toISOString();
+            showTimestampGMT(timestamp, e.pageX, e.pageY, decodedTS, selection);
+          }
+        }
+
+        function showTimestampGMT(timestamp, xPos, yPos, decodedTS, selection) {
+          timestamp.innerText = decodedTS;
+          timestamp.style.top = yPos + 'px';
+          timestamp.style.left = xPos + 'px';
+          timestamp.className = "timestamp_gmt timestamp_icn";
+          timestamp.style.visibility = 'visible';
+          console.log("Timestamp Converter GMT: ", selection, decodedTS);
+        }
+      }
+    } else {
+      // When clicking the timestamp window, copy content to clipboard
+      navigator.clipboard.writeText(e.target.innerText).then(function () {
+        console.log("Timestamp Converter GMT: Sent to clipboard.", e.target.innerText);
+        // rotate image as visual cue via CSS
+        e.target.classList.toggle("timestamp_icn")
+        e.target.classList.toggle("timestamp_clp")
+      }, function (err) {
+        console.error('Timestamp Converter GMT: Failed to send to clipboard.', err);
+      });
+    }
+  }, false);
+
+  document.addEventListener('mousedown', function (e) {
+    if (!e.target.matches('.timestamp_gmt')) {
+      timestamp.style.visibility = 'hidden';
+      var selection = window.getSelection();
+      if (!!selection) {
+        selection.removeAllRanges();
       }
     }
-  }
-}, false);
-
-document.addEventListener('mousedown', function(e) {
-  // Check target to enable mark-then-copy of the decoded timestamp
-  if(e.target != timestamp) {
-    timestamp.style.visibility = 'hidden';
-  }
-}, false);
-
-function showTimestamp(xPos, yPos, decodedTS) {
-  timestamp.innerHTML = decodedTS;
-  timestamp.style.top = yPos + 'px';
-  timestamp.style.left = xPos + 'px';
-  timestamp.style.visibility = 'visible';
-}
+  }, false);
+})()
